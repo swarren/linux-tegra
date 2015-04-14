1590e3571fa8e63ba7dc0e810f5e62008c16da04
Stephen Warren <swarren@nvidia.com>
2015-04-14 10:39:30 -0600

    HACK: convert latency tester to ARM arch timers
    This way, we know exactly when the HW triggered the IRQ, so can get more
    exact latency values. Also, since we're scheduling IRQs from kernel space,
    we're quite unlikely to drop IRQs.
    
    TODO:
    * Add a real queue of timestamps into the driver, so we can increase the
      IRQ frequency a lot, even to the point where user-space doesn't get run
      for each separate IRQ.
    * Easier control over which CPU's timer triggers the IRQ; IOCTL or write
      data?
    
    Signed-off-by: Stephen Warren <swarren@nvidia.com>

--- drivers/soc/tegra/latency-tester.c
+++ drivers/soc/tegra/latency-tester.c
@@ -9,70 +9,165 @@
 #include <linux/irq.h>
 #include <linux/module.h>
 #include <linux/of_device.h>
+#include <linux/of_irq.h>
 #include <linux/platform_device.h>
+#include <linux/sched.h>
+#include <linux/spinlock.h>
+#include <linux/wait.h>
+
+#define ARCH_TIMER_CTRL_ENABLE		(1 << 0)
+#define ARCH_TIMER_CTRL_IT_MASK		(1 << 1)
 
 struct latency_tester {
-	void __iomem *trigger_reg;
-	u32 trigger_val;
+	struct latency_tester __percpu **lt_percpu;
 	int irq;
 	struct dentry *debugfs;
-
-	struct completion irq_done;
+	bool open;
+	bool data_in_queue;
+	spinlock_t lock;
+	wait_queue_head_t wait;
+	u32 cntfrq;
+	int timer_cpu;
+	u64 last_cval;
 	u64 time_trigger;
 	u64 time_isr;
 };
 
-static inline u64 arch_counter_get_cntvct(void)
+static inline u32 read_cntfrq(void)
 {
-	u64 cval;
+	u32 val;
+
+	asm volatile("mrc p15, 0, %0, c14, c0, 0" : "=r" (val));
+
+	return val;
+}
+
+static inline u64 read_cntvct(void)
+{
+	u32 cval_lo;
+	u32 cval_hi;
 
 	isb();
-	asm volatile("mrrc p15, 1, %Q0, %R0, c14" : "=r" (cval));
-	return cval;
+	asm volatile("mrrc p15, 1, %0, %1, c14" : "=r" (cval_lo), "=r" (cval_hi));
+
+	return (((u64)cval_hi) << 32) | cval_lo;
+}
+
+static inline void write_cntv_ctl(u32 val)
+{
+	asm volatile("mcr p15, 0, %0, c14, c3, 1" : : "r" (val));
+	isb();
+}
+
+static inline void write_cntv_cval(u64 val)
+{
+	asm volatile("mcrr p15, 3, %0, %1, c14" : : "r" (val & 0xffffffff), "r" (val >> 32));
+	isb();
+}
+
+static inline void set_timer_expiry(struct latency_tester *lt)
+{
+	lt->last_cval += lt->cntfrq;
+	write_cntv_cval(lt->last_cval);
+}
+
+static inline void enable_timer(struct latency_tester *lt)
+{
+	lt->timer_cpu = smp_processor_id();
+
+	lt->last_cval = read_cntvct();
+	set_timer_expiry(lt);
+
+	write_cntv_ctl(ARCH_TIMER_CTRL_ENABLE);
+	enable_percpu_irq(lt->irq, 0);
+}
+
+static void disable_timer_imp(void *arg)
+{
+	struct latency_tester *lt = arg;
+
+	disable_percpu_irq(lt->irq);
+	write_cntv_cval(0xffffffffffffffffULL);
+	write_cntv_ctl(ARCH_TIMER_CTRL_IT_MASK);
+}
+
+static inline void disable_timer(struct latency_tester *lt)
+{
+	smp_call_function_single(lt->timer_cpu, disable_timer_imp, lt, 1);
 }
 
 static int lt_open(struct inode *inode, struct file *file)
 {
+	struct latency_tester *lt = file->f_inode->i_private;
+	unsigned long flags;
+	int ret;
+
+	spin_lock_irqsave(&lt->lock, flags);
+	if (lt->open) {
+		ret = -EBUSY;
+	} else {
+		lt->open = true;
+		ret = 0;
+	}
+	spin_unlock_irqrestore(&lt->lock, flags);
+	if (ret)
+		return ret;
+
+	enable_timer(lt);
+
 	return nonseekable_open(inode, file);
+}
+
+static int lt_release(struct inode *inode, struct file *file)
+{
+	struct latency_tester *lt = file->f_inode->i_private;
+
+	disable_timer(lt);
+	lt->open = false;
+
+	return 0;
 }
 
 ssize_t lt_write(struct file *file, const char __user *data,
 		   size_t len, loff_t *ppos)
 {
-	struct latency_tester *lt = file->f_inode->i_private;
-
-	lt->time_trigger = arch_counter_get_cntvct();
-	writel(lt->trigger_val, lt->trigger_reg);
-
-	return len;
+	return -EINVAL;
 }
 
 ssize_t lt_read(struct file *file, char __user *buf,
 		  size_t len, loff_t *ppos)
 {
 	struct latency_tester *lt = file->f_inode->i_private;
+	unsigned long flags;
 	int ret;
 
 	if (len != 16)
 		return -EINVAL;
 
-	reinit_completion(&lt->irq_done);
-	ret = wait_for_completion_interruptible(&lt->irq_done);
-	if (ret < 0)
-		return -EIO;
+	for (;;) {
+		if (lt->data_in_queue) {
+			spin_lock_irqsave(&lt->lock, flags);
+			ret = copy_to_user(buf, &lt->time_trigger, len);
+			if (!ret)
+				lt->data_in_queue = false;
+			spin_unlock_irqrestore(&lt->lock, flags);
+			if (ret)
+				return ret;
 
-	ret = copy_to_user(buf, &lt->time_trigger, len);
-	if (ret < 0)
-		return ret;
+			*ppos += len;
+			return len;
+		}
 
-	*ppos += len;
-
-	return len;
+		ret = wait_event_interruptible(lt->wait, lt->data_in_queue);
+		if (ret)
+			return ret;
+	}
 }
 
 static const struct file_operations lt_fileops = {
 	.owner	= THIS_MODULE,
 	.open	= lt_open,
+	.release = lt_release,
 	.write	= lt_write,
 	.read	= lt_read,
 	.llseek = no_llseek,
@@ -80,10 +175,20 @@ static const struct file_operations lt_fileops = {
 
 static irqreturn_t latency_tester_isr(int irq, void *arg)
 {
-	struct latency_tester *lt = arg;
+	struct latency_tester **ltp = arg;
+	struct latency_tester *lt = *ltp;
+	unsigned long flags;
 
-	lt->time_isr = arch_counter_get_cntvct();
-	complete(&lt->irq_done);
+	spin_lock_irqsave(&lt->lock, flags);
+	if (!lt->data_in_queue) {
+		lt->time_trigger = lt->last_cval;
+		lt->time_isr = read_cntvct();
+		lt->data_in_queue = true;
+		wake_up_interruptible(&lt->wait);
+	}
+	spin_unlock_irqrestore(&lt->lock, flags);
+
+	set_timer_expiry(lt);
 
 	return IRQ_HANDLED;
 }
@@ -91,8 +196,7 @@ static irqreturn_t latency_tester_isr(int irq, void *arg)
 static int latency_tester_probe(struct platform_device *pdev)
 {
 	struct latency_tester *lt;
-	int ret;
-	struct resource *mem;
+	int cpu, ret;
 
 	lt = devm_kzalloc(&pdev->dev, sizeof(struct latency_tester), GFP_KERNEL);
 	if (!lt) {
@@ -101,38 +205,31 @@ static int latency_tester_probe(struct platform_device *pdev)
 	}
 	dev_set_drvdata(&pdev->dev, lt);
 
-	init_completion(&lt->irq_done);
+	spin_lock_init(&lt->lock);
+	init_waitqueue_head(&lt->wait);
+	lt->cntfrq = read_cntfrq();
 
-	mem = platform_get_resource(pdev, IORESOURCE_MEM, 0);
-	if (!mem) {
-		dev_err(&pdev->dev, "No memory resource\n");
-		ret = -ENODEV;
-		goto err;
-	}
+	disable_timer(lt);
 
-	lt->trigger_reg = devm_ioremap(&pdev->dev, mem->start, resource_size(mem));
-	if (!lt->trigger_reg) {
-		dev_err(&pdev->dev, "ioremap failed\n");
-		ret = -ENOMEM;
-		goto err;
-	}
-
-	ret = of_property_read_u32(pdev->dev.of_node, "trigger-val", &lt->trigger_val);
-	if (ret < 0) {
-		dev_err(&pdev->dev, "Failed to read trigger-val: %d\n", ret);
-		goto err;
-	}
-
-	lt->irq = platform_get_irq(pdev, 0);
+	lt->irq = irq_of_parse_and_map(pdev->dev.of_node, 0);
 	if (lt->irq < 0) {
+		ret = lt->irq;
 		dev_err(&pdev->dev, "failed to get IRQ: %d\n", ret);
 		goto err;
 	}
 
-	ret = request_irq(lt->irq, latency_tester_isr, IRQF_SHARED, "latency-tester", lt);
-	if (ret) {
-		dev_err(&pdev->dev, "failed to register IRQ: %d\n", ret);
+	lt->lt_percpu = alloc_percpu(struct latency_tester *);
+	if (!lt->lt_percpu) {
+		ret = -ENOMEM;
 		goto err;
+	}
+	for_each_possible_cpu(cpu)
+		*per_cpu_ptr(lt->lt_percpu, cpu) = lt;
+
+	ret = request_percpu_irq(lt->irq, latency_tester_isr, "latency-tester", lt->lt_percpu);
+	if (ret) {
+		dev_err(&pdev->dev, "failed to register IRQ %d: %d\n", lt->irq, ret);
+		goto err_free_percpu;
 	}
 
 	lt->debugfs = debugfs_create_file("latency_tester", S_IFREG | S_IRUGO | S_IWUGO, NULL, lt, &lt_fileops);
@@ -144,7 +241,9 @@ static int latency_tester_probe(struct platform_device *pdev)
 	return 0;
 
 err_free_irq:
-	free_irq(lt->irq, lt);
+	free_percpu_irq(lt->irq, lt);
+err_free_percpu:
+	free_percpu(lt->lt_percpu);
 err:
 	return ret;
 }
@@ -154,7 +253,8 @@ static int latency_tester_remove(struct platform_device *pdev)
 	struct latency_tester *lt = dev_get_drvdata(&pdev->dev);
 
 	debugfs_remove(lt->debugfs);
-	free_irq(lt->irq, lt);
+	free_percpu_irq(lt->irq, lt);
+	free_percpu(lt->lt_percpu);
 
 	return 0;
 }
